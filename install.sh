de902de58aa52cc95f905851f2803f13e94bc621
TheDarkTroopers <100459961+TheDarkTroopers@users.noreply.github.com>
2023-01-16 01:01:26 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lblue="\e94m"
+
+echo -e "${blue}Instalando cosillas jijiji...."
+
+pip install requests
+pip install json
+pip install colorama
+pip install time
+pip install pycryptodome
+
+echo -e "${blue}Se instalaron con exito :D"
