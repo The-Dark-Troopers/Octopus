7e9b6689b6b5685636f31e182906429787d04a24
Defalt Markowickz <85587928+Defalt-Markowickz@users.noreply.github.com>
2023-02-28 09:12:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ lblue="\e94m"
 
 echo -e "${blue}Instalando cosillas jijiji...."
 
-pip install requests
-pip install json
-pip install colorama
-pip install time
-pip install pycryptodome
+pip install pyfiglet
 
 echo -e "${blue}Se instalaron con exito :D"
